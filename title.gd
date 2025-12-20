d01234370ba2fa96bdf9ae409c715f1102984058
RTalaro <146395601+RTalaro@users.noreply.github.com>
2025-12-19 23:55:53 -0800

    feat: implement play/quit title buttons

---
+++ title.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+@onready var play = $VBoxContainer2/Buttons/Play
+@onready var options = $VBoxContainer2/Buttons/Options
+@onready var quit = $VBoxContainer2/Buttons/Quit
+
+func _ready():
+	play.connect("button_down", on_play_button_down)
+	quit.connect("button_down", on_quit_button_down)
+
+
+func on_play_button_down():
+	get_tree().change_scene_to_file("res://level.tscn")
+
+func on_quit_button_down():
+	get_tree().quit()
